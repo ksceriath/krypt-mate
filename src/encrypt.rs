865e30eb886795befd877d7edbd1e1a7f5eed89f
ksceriath <kxzorro@gmail.com>
2020-07-26 16:58:40 +0530

    small refactor of encrypt.rs

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -1,13 +1,13 @@
-use crate::hexaa;
 use crate::encodings;
+use crate::hexaa;
 
 pub fn repeated_key_xor(hex: &str, key: &str) -> String {
     let bytes: Vec<u8> = hex.as_bytes().to_vec();
     let key: Vec<u8> = key.as_bytes().to_vec();
-    let mut repeated_key = Vec::new();
-    for i in 0..bytes.len() {
-        repeated_key.push(key[i % key.len()]);
-    }
+    let repeated_key = bytes
+        .chunks(key.len())
+        .flat_map(|chunk| chunk.iter().zip(key.clone()).map(|(_, key_byte)| key_byte))
+        .collect();
     let encrypted = hexaa::xor_bytes(&bytes, &repeated_key);
     encodings::bytes_to_hex(&encrypted)
 }
