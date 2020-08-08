4b739cfcc51294e9d0d4344c8f4bf37012d01fbd
ksceriath <kxzorro@gmail.com>
2020-08-09 00:10:37 +0530

    refactor: split repeated_key_xor method

--- src/bin/challenge5.rs
+++ src/bin/challenge5.rs
@@ -4,7 +4,10 @@ fn main() {
     env_logger::init();
     let text = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
     let key = "ICE";
-    println!("Encrypted : {}", encrypt::repeated_key_xor(text, key));
+    println!(
+        "Encrypted : {}",
+        encrypt::repeated_key_xor_with_strings(text, key)
+    );
 }
 
 // 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
--- src/encrypt.rs
+++ src/encrypt.rs
@@ -1,13 +1,17 @@
 use crate::encodings;
 use crate::hexaa;
 
-pub fn repeated_key_xor(hex: &str, key: &str) -> String {
+pub fn repeated_key_xor_with_strings(hex: &str, key: &str) -> String {
     let bytes: Vec<u8> = hex.as_bytes().to_vec();
     let key: Vec<u8> = key.as_bytes().to_vec();
+    let encrypted = repeated_key_xor(&bytes, &key);
+    encodings::bytes_to_hex(&encrypted)
+}
+
+pub fn repeated_key_xor(bytes: &Vec<u8>, key: &Vec<u8>) -> Vec<u8> {
     let repeated_key = bytes
         .chunks(key.len())
         .flat_map(|chunk| chunk.iter().zip(key.clone()).map(|(_, key_byte)| key_byte))
         .collect();
-    let encrypted = hexaa::xor_bytes(&bytes, &repeated_key);
-    encodings::bytes_to_hex(&encrypted)
+    hexaa::xor_bytes(&bytes, &repeated_key)
 }
